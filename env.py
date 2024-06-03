c4b2651933c8560d6e1f0dfbda58b87f53985221
LEGEND X <legendxx08377@gmail.com>
2024-06-03 16:24:54 +0530

    ad added

--- env.py
+++ env.py
@@ -8,3 +8,36 @@ MONGO_URI = "mongodb+srv://legendx:legendx22@bgmi.awpnsir.mongodb.net/?retryWrit
 telegram_database_chat = "@testingbot_22"
 debug_channel_id = "@channelforsavingcontent"
 
+
+
+TELEGRAM_AD = """
+🚀 Introducing AP ADVERTISEMENT BOT! 🚀
+
+Hey everyone! We're excited to launch our new bot, @AP_ADVERTISEMENTS_BOT. This powerful tool is packed with features and comes at an incredibly affordable price.
+
+Quick Overview:
+
+1. Add Telegram IDs:  
+   Add as many Telegram IDs as you need (no limitation currently).
+
+2. Join Groups:  
+   Use the bot to join multiple groups individually with these IDs.
+
+3. Auto Post:  
+   Utilize the 'auto post' feature to send your ad/message to all groups across all added IDs simultaneously. For instance, if you add 3 IDs and each is in 50 groups, all IDs will start broadcasting your ad to their respective 50 groups at the same time without any delay. After broadcasting, the bot will take a break (duration set by you) and then resume posting automatically. Simply set it up once, and the bot will handle the rest!
+
+Main Features:
+
+- 🌟 AUTO POST
+- 🌟 AUTO GROUP JOINER
+- 🌟 ANTI BAN / ANTI FLOOD
+
+Plans and Pricing:
+
+- Starter Plan: ₹430 INR/month - Includes 2 complimentary IDs
+- Pro Plan: ₹800 INR/2 months - Includes 5 complimentary IDs
+
+Experience seamless advertising with AP ADVERTISEMENT BOT. Get started now and let the bot do the work for you!
+
+📲 Contact us: @APHACKAR
+""".strip()
