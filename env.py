2a073b32022e9863910cdf981536ed78d5e794a2
Atharv Patil <116619280+atharvtg2252@users.noreply.github.com>
2026-02-01 17:35:07 +0530

    Update env.py

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ SUDO_USERS = []
 api_id = 1621727
 api_hash = "31350903c528876f79527398c09660ce"
 bot_token = "7163325940:AAG6EqYkwvfbpZiq8ME9Rbvp4U9VCHhr4DQ"
-MONGO_URI = "mongodb+srv://legendx:legendx22@bgmi.awpnsir.mongodb.net/?retryWrites=true&w=majority&appName=BGMI "
+MONGO_URI = "mongodb+srv://aphackerff_db_user:YUMvtyOJlNmWIOtK@cluster0.tljshns.mongodb.net/?appName=Cluster0 "
 telegram_database_chat = "@testingbot_22"
 debug_channel_id = "@channelforsavingcontent"
 
